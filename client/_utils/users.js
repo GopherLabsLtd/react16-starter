994c689e72d1ca57c6e4f3db4fd906594629aeb2
The Iliad <iliadmoosavi@gmail.com>
2017-10-05 14:56:55 -0400

    Added users JSON mock file

---
+++ client/_utils/users.js
@@ -0,0 +1,183 @@
+module.exports = [
+    {
+        "gender": "male",
+        "name": {
+            "title": "mr",
+            "first": "romain",
+            "last": "hoogmoed"
+        },
+        "location": {
+            "street": "1861 jan pieterszoon coenstraat",
+            "city": "maasdriel",
+            "state": "zeeland",
+            "postcode": 69217
+        },
+        "email": "romain.hoogmoed@example.com",
+        "login": {
+            "username": "lazyduck408",
+            "password": "jokers",
+            "salt": "UGtRFz4N",
+            "md5": "6d83a8c084731ee73eb5f9398b923183",
+            "sha1": "cb21097d8c430f2716538e365447910d90476f6e",
+            "sha256": "5a9b09c86195b8d8b01ee219d7d9794e2abb6641a2351850c49c309f1fc204a0"
+        },
+        "dob": "1983-07-14 07:29:45",
+        "registered": "2010-09-24 02:10:42",
+        "phone": "(656)-976-4980",
+        "cell": "(065)-247-9303",
+        "id": {
+            "name": "BSN",
+            "value": "04242023"
+        },
+        "picture": {
+            "large": "https://randomuser.me/api/portraits/men/83.jpg",
+            "medium": "https://randomuser.me/api/portraits/med/men/83.jpg",
+            "thumbnail": "https://randomuser.me/api/portraits/thumb/men/83.jpg"
+        },
+        "nat": "NL"
+    }, {
+        "gender": "female",
+        "name": {
+          "title": "mrs",
+          "first": "tracy",
+          "last": "miles"
+        },
+        "location": {
+          "street": "6078 park avenue",
+          "city": "cardiff",
+          "state": "tyne and wear",
+          "postcode": "J75 2AF"
+        },
+        "email": "tracy.miles@example.com",
+        "login": {
+          "username": "tinybutterfly423",
+          "password": "citroen",
+          "salt": "gxuGaQ00",
+          "md5": "96a384d931afb8a4dcd9b9c333529fc9",
+          "sha1": "307f522d9b570ded401186dc36bb769ca756cfc6",
+          "sha256": "f2b38e9e035438bb63a2cb6bb32ee3033afa195c132f193caa47ab3dbd221130"
+        },
+        "dob": "1969-02-27 15:17:08",
+        "registered": "2003-04-04 11:50:58",
+        "phone": "015242 75958",
+        "cell": "0744-163-340",
+        "id": {
+          "name": "NINO",
+          "value": "WT 75 86 67 W"
+        },
+        "picture": {
+          "large": "https://randomuser.me/api/portraits/women/81.jpg",
+          "medium": "https://randomuser.me/api/portraits/med/women/81.jpg",
+          "thumbnail": "https://randomuser.me/api/portraits/thumb/women/81.jpg"
+        },
+        "nat": "GB"
+    }, {
+        "gender": "female",
+        "name": {
+          "title": "miss",
+          "first": "gladys",
+          "last": "johnston"
+        },
+        "location": {
+          "street": "5448 brown terrace",
+          "city": "rialto",
+          "state": "florida",
+          "postcode": 63712
+        },
+        "email": "gladys.johnston@example.com",
+        "login": {
+          "username": "organicpeacock870",
+          "password": "venom",
+          "salt": "vifV1h7K",
+          "md5": "7d4bc759928f4283f48889ec2a970b16",
+          "sha1": "59702b8592ca2a24e217bc8d389db902ed32592f",
+          "sha256": "c4198c6973790a4b63a5c64fa07ddd1f68b44d72a49947639c62b08ee697a8ec"
+        },
+        "dob": "1973-06-28 00:45:56",
+        "registered": "2007-09-05 16:01:00",
+        "phone": "(184)-508-0371",
+        "cell": "(224)-257-8047",
+        "id": {
+          "name": "SSN",
+          "value": "229-20-9203"
+        },
+        "picture": {
+          "large": "https://randomuser.me/api/portraits/women/91.jpg",
+          "medium": "https://randomuser.me/api/portraits/med/women/91.jpg",
+          "thumbnail": "https://randomuser.me/api/portraits/thumb/women/91.jpg"
+        },
+        "nat": "US"
+    }, {
+        "gender": "female",
+        "name": {
+          "title": "miss",
+          "first": "ülkü",
+          "last": "abacı"
+        },
+        "location": {
+          "street": "8578 vatan cd",
+          "city": "zonguldak",
+          "state": "kırıkkale",
+          "postcode": 64854
+        },
+        "email": "ülkü.abacı@example.com",
+        "login": {
+          "username": "bigduck790",
+          "password": "pornking",
+          "salt": "KcfFTqGR",
+          "md5": "63d09bf39a486b79dd9cbcb519c0b93f",
+          "sha1": "61e43130d6c5b1a8a0ec1a245315affa8951ac64",
+          "sha256": "bf6260ec3feab099bbc8989362949b877037c5cc78ba3c747c9904ce362151e3"
+        },
+        "dob": "1963-04-26 05:43:03",
+        "registered": "2015-11-11 14:04:37",
+        "phone": "(890)-306-3556",
+        "cell": "(915)-236-4290",
+        "id": {
+          "name": "",
+          "value": null
+        },
+        "picture": {
+          "large": "https://randomuser.me/api/portraits/women/42.jpg",
+          "medium": "https://randomuser.me/api/portraits/med/women/42.jpg",
+          "thumbnail": "https://randomuser.me/api/portraits/thumb/women/42.jpg"
+        },
+        "nat": "TR"
+    }, {
+        "gender": "male",
+        "name": {
+          "title": "mr",
+          "first": "darius",
+          "last": "witt"
+        },
+        "location": {
+          "street": "6455 blumenstraße",
+          "city": "koblenz",
+          "state": "saarland",
+          "postcode": 94775
+        },
+        "email": "darius.witt@example.com",
+        "login": {
+          "username": "redmouse226",
+          "password": "custom",
+          "salt": "vQBwgR2G",
+          "md5": "4b701561c619b23e5fb7d89206885665",
+          "sha1": "45225fb76625136e4a2275edd2b3e6c0a94e61a7",
+          "sha256": "9ca66e34ebe14e6752da341c3160ca5dba8164e3ce720527d8c656862abce333"
+        },
+        "dob": "1988-08-31 23:31:56",
+        "registered": "2014-08-27 07:15:13",
+        "phone": "0380-7445241",
+        "cell": "0170-7756763",
+        "id": {
+          "name": "",
+          "value": null
+        },
+        "picture": {
+          "large": "https://randomuser.me/api/portraits/men/75.jpg",
+          "medium": "https://randomuser.me/api/portraits/med/men/75.jpg",
+          "thumbnail": "https://randomuser.me/api/portraits/thumb/men/75.jpg"
+        },
+        "nat": "DE"
+    }
+];
